ab50f1d912ea083327c44007a03160f7265205ec
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-14 23:51:46 -0300

    feat: execute_query postgres (rust)

--- rust_drivers/postgres/src/lib.rs
+++ rust_drivers/postgres/src/lib.rs
@@ -1,5 +1,5 @@
 use postgres::{Client, NoTls};
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::ptr;
 
@@ -30,10 +30,43 @@ pub extern "C" fn free_postgresql_connection(client: *mut Client) {
     }
 }
 
+pub extern "C" fn execute_query(client_ptr: *mut Client, query: *const c_char) -> *mut c_char {
+    if client_ptr.is_null() || query.is_null() {
+        let error_msg = CString::new("Invalid client or query").unwrap();
+        return error_msg.into_raw();
+    }
+
+    let client = unsafe { &mut *client_ptr };
+    let c_str = unsafe { CStr::from_ptr(query) };
+    let query_str = match c_str.to_str() {
+        Ok(s) => s,
+        Err(_) => {
+            let error_msg = CString::new("Invalid UTF-8 in query").unwrap();
+            return error_msg.into_raw();
+        }
+    };
+
+    match client.query(query_str, &[]) {
+        Ok(rows) => {
+            let result = CString::new(format!("Found {} rows", rows.len())).unwrap();
+            result.into_raw()
+        }
+        Err(e) => {
+            let error_msg = CString::new(format!("Failed to execute query: {}", e)).unwrap();
+            error_msg.into_raw()
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{connect_to_postgresql, free_postgresql_connection};
-    use std::{env, ffi::CString};
+    use postgres::Client;
+
+    use crate::{connect_to_postgresql, execute_query, free_postgresql_connection};
+    use std::{
+        env,
+        ffi::{CStr, CString},
+    };
 
     #[test]
     fn test_postgresql_connection_success() {
@@ -66,5 +99,30 @@ mod tests {
             result.is_null(),
             "The connection should fail and return a null pointer"
         );
+    }
+
+    fn create_test_client() -> *mut Client {
+        let connection_url = env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL not set");
+        let c_connection_url = CString::new(connection_url).unwrap();
+        let ptr_connection_url = c_connection_url.as_ptr();
+
+        connect_to_postgresql(ptr_connection_url)
+    }
+
+    #[test]
+    fn test_execute_query_success() {
+        let client_ptr = create_test_client();
+
+        let query = CString::new("SELECT * FROM users").unwrap();
+        let query_ptr = query.as_ptr();
+
+        let result_ptr = execute_query(client_ptr, query_ptr);
+
+        let result_str = unsafe { CStr::from_ptr(result_ptr).to_str().unwrap() };
+
+        print!("{}", result_str);
+
+        assert!(result_str.contains("Found"));
+        assert!(result_str.contains("rows"));
     }
 }
