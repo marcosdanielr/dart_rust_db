c7cfb72c23dac745e4293165c6a245a18f39d0cf
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-14 23:36:08 -0300

    fix: return postgres client (rust)

--- rust_drivers/postgres/src/lib.rs
+++ rust_drivers/postgres/src/lib.rs
@@ -1,38 +1,38 @@
 use postgres::{Client, NoTls};
 use std::ffi::CStr;
 use std::os::raw::c_char;
+use std::ptr;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn connect_to_postgresql(connection_url: *const c_char) -> bool {
+pub extern "C" fn connect_to_postgresql(connection_url: *const c_char) -> *mut Client {
     if connection_url.is_null() {
-        eprintln!("Connection URL is null");
-        return false;
+        return ptr::null_mut();
     }
 
     let c_str = unsafe { CStr::from_ptr(connection_url) };
     let conn_str = match c_str.to_str() {
         Ok(s) => s,
-        Err(_) => {
-            eprintln!("Invalid UTF-8 in connection string");
-            return false;
-        }
+        Err(_) => return ptr::null_mut(),
     };
 
     match Client::connect(conn_str, NoTls) {
-        Ok(_) => {
-            println!("Connected successfully");
-            true
-        }
-        Err(e) => {
-            eprintln!("Failed to connect: {}", e);
-            false
+        Ok(client) => Box::into_raw(Box::new(client)),
+        Err(_) => ptr::null_mut(),
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn free_postgresql_connection(client: *mut Client) {
+    if !client.is_null() {
+        unsafe {
+            let _ = Box::from_raw(client);
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::connect_to_postgresql;
+    use crate::{connect_to_postgresql, free_postgresql_connection};
     use std::{env, ffi::CString};
 
     #[test]
@@ -43,9 +43,14 @@ mod tests {
 
         let ptr_connection_url = c_connection_url.as_ptr();
 
-        let success = connect_to_postgresql(ptr_connection_url);
+        let result = connect_to_postgresql(ptr_connection_url);
 
-        assert!(success, "Failed to connect to the PostgreSQL");
+        assert!(
+            !result.is_null(),
+            "The connection should succeed and return a non-null pointer"
+        );
+
+        free_postgresql_connection(result);
     }
 
     #[test]
@@ -55,8 +60,11 @@ mod tests {
         let c_invalid_url = CString::new(invalid_url).expect("Failed to create CString");
         let ptr_invalid_url = c_invalid_url.as_ptr();
 
-        let success = connect_to_postgresql(ptr_invalid_url);
+        let result = connect_to_postgresql(ptr_invalid_url);
 
-        assert!(!success, "The connection should fail with an invalid URL");
+        assert!(
+            result.is_null(),
+            "The connection should fail and return a null pointer"
+        );
     }
 }
